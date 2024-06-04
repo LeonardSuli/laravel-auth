a49fb7bf9039bc246c4834616305c0b53bfaaaba
LeonardSuli <leonardsuli1991@gmail.com>
2024-06-04 18:11:16 +0200

    join project and type model with foreign key

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'cover_image', 'project_url', 'source_code_url', 'description'];
+
+    /**
+     * Get the type that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
